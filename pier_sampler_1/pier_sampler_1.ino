e1782bb9c490a1663dd40eb34bc1cc38f7fd54cb
ari3010 <arindamchatterji@Arindams-MacBook-Air.local>
2017-04-17 10:09:43 -0700

    Code with updated comments

--- pier_sampler_1/pier_sampler_1.ino
+++ pier_sampler_1/pier_sampler_1.ino
@@ -1,23 +1,24 @@
-//initialize solenoid valves
-int sol_A = 43;  
-int sol_B = 45;
-int sol_D = 47;
-int sol_C = 6;
-int sol_X1 = 2;
-int sol_X2 = 3;
-int sol_X3 = 4;
-int sol_X4 = 5;
+//initialize solenoid valves for arduino digital pins
+int sol_A = 4;  
+int sol_B = 3;
+int sol_D = 2;
+int sol_C = 12;
+int sol_X1 = 11;
+int sol_X2 = 10;
+int sol_X3 = 9;
+int sol_X4 = 8;
 int sol_X5 = 7;
-int sol_X6 = 8;
-int sol_X7 = 9;
+int sol_X6 = 6;
+int sol_X7 = 5;
 int sampling = 0;
 
 //added temp sensor
 int temp_sensor = 5;
 void setup() {
-  // put your setup code here, to run once:
 
+  //begin Serial monitor 
 Serial.begin(9600);
+//set all solenoids to output
 pinMode(sol_A, OUTPUT);
 pinMode(sol_B, OUTPUT);
 pinMode(sol_D, OUTPUT);
@@ -42,19 +43,24 @@ float temp_C = (voltage- 0.5) * 100; //celcius temp
 Serial.print(temp_C);
 //if (temp_C <20) //if in normal condition
 if (sampling == 0) //if in normal condition
-{
-  //normal operation
+
+ //normal operation
 digitalWrite(sol_A, HIGH);
 digitalWrite(sol_B, HIGH);
 digitalWrite(sol_D, HIGH);
 delay(5000);
 //begin sampling
+
 //X opens
 digitalWrite(sol_X1, HIGH);
 digitalWrite(sol_X2, HIGH);
 digitalWrite(sol_X3, HIGH);
 digitalWrite(sol_X4, HIGH);
-delay(5000);
+digitalWrite(sol_X5, HIGH);
+digitalWrite(sol_X6, HIGH);
+digitalWrite(sol_X7, HIGH);
+delay(10000);
+
 //B and D close
 digitalWrite(sol_B, LOW);
 digitalWrite(sol_D, LOW);
@@ -79,15 +85,17 @@ digitalWrite(sol_X7, LOW);
 delay(1000);
 
 //C closes
-digitalWrite(sol_A, LOW);
+digitalWrite(sol_C, LOW);
 
 //A opens
-digitalWrite(sol_C, HIGH);
+digitalWrite(sol_A, HIGH);
 
 //B and D open
 digitalWrite(sol_B, HIGH);
 digitalWrite(sol_D, HIGH);
-//sampling++;
+
+
+delay(1000);
 }
 
 
