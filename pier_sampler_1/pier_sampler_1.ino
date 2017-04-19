21811541e0e46a7d0fff0c72bf66bd84d42d2c4c
ari3010 <arindamchatterji@Arindams-MacBook-Air.local>
2017-04-19 13:45:55 -0700

    update

--- pier_sampler_1/pier_sampler_1.ino
+++ pier_sampler_1/pier_sampler_1.ino
@@ -1,90 +1,55 @@
-//initialize solenoid valves for arduino digital pins
-int sol_A = 4;  
-int sol_B = 3;
-int sol_D = 2;
-int sol_C = 12;
-int sol_X1 = 11;
-int sol_X2 = 10;
-int sol_X3 = 9;
-int sol_X4 = 8;
-int sol_X5 = 7;
-int sol_X6 = 6;
-int sol_X7 = 5;
-int sampling = 0;
+#include <elapsedMillis.h>
+int sol_B = 22; 
+int sol_C = 4;  
+int sol_D = 3; 
+int sol_A = 12; 
 
-//added temp sensor
-int temp_sensor = 5;
+elapsedMillis timer0;
+#define interval 3000
+  
 void setup() {
-
-  //begin Serial monitor 
-Serial.begin(9600);
-//set all solenoids to output
-pinMode(sol_A, OUTPUT);
+timer0=0;;  
+for(int solX_Pin = 5; solX_Pin < 12; solX_Pin++)
+{
+  pinMode(solX_Pin, OUTPUT);
+}pinMode(sol_A, OUTPUT);
 pinMode(sol_B, OUTPUT);
 pinMode(sol_D, OUTPUT);
-pinMode(sol_X1, OUTPUT);
-pinMode(sol_X2, OUTPUT);
-pinMode(sol_X3, OUTPUT);
-pinMode(sol_X4, OUTPUT);
-pinMode(sol_X5, OUTPUT);
-pinMode(sol_X6, OUTPUT);
-pinMode(sol_X7, OUTPUT);
+pinMode(sol_C, OUTPUT);
 }
 
+
+
+
 void loop() {
-  //integrate temp sensor
-int reading = analogRead(temp_sensor);
-
-//analog to voltage value
-float voltage = reading * 5.0;
-voltage /= 1024.0;
-float temp_C = (voltage- 0.5) * 100; //celcius temp
-
-Serial.print(temp_C);
-//if (temp_C <20) //if in normal condition
-if (sampling == 0) //if in normal condition
-
- //normal operation
+  // put your main code here, to run repeatedly:
 digitalWrite(sol_A, HIGH);
 digitalWrite(sol_B, HIGH);
 digitalWrite(sol_D, HIGH);
+
 delay(5000);
-//begin sampling
 
+
+  //sampling
+
+for(int solX_Pin = 5; solX_Pin < 12 &&timer0 >interval; solX_Pin++){
 //X opens
-digitalWrite(sol_X1, HIGH);
-digitalWrite(sol_X2, HIGH);
-digitalWrite(sol_X3, HIGH);
-digitalWrite(sol_X4, HIGH);
-digitalWrite(sol_X5, HIGH);
-digitalWrite(sol_X6, HIGH);
-digitalWrite(sol_X7, HIGH);
-delay(10000);
-
-//B and D close
+digitalWrite(solX_Pin, HIGH);
+ //delay(timer);
+//B and D close 
 digitalWrite(sol_B, LOW);
 digitalWrite(sol_D, LOW);
-delay(5000);
 
-//A closes
+//A closes 
 digitalWrite(sol_A, LOW);
 
-//C opens
+//C opens 
 digitalWrite(sol_C, HIGH);
-delay(5000);
 
 //X closes
-digitalWrite(sol_X1, LOW);
-digitalWrite(sol_X2, LOW);
-digitalWrite(sol_X3, LOW);
-digitalWrite(sol_X4, LOW);
-digitalWrite(sol_X5, LOW);
-digitalWrite(sol_X6, LOW);
-digitalWrite(sol_X7, LOW);
+digitalWrite(solX_Pin, LOW);
 
-delay(1000);
-
-//C closes
+//C closes 
 digitalWrite(sol_C, LOW);
 
 //A opens
@@ -94,8 +59,8 @@ digitalWrite(sol_A, HIGH);
 digitalWrite(sol_B, HIGH);
 digitalWrite(sol_D, HIGH);
 
-
-delay(1000);
-}
+delay(5000);
+  }
+ }
 
 
